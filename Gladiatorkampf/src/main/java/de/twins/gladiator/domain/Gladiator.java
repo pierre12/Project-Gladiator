d83070ec754db22eaf9e931eb3c7806182163b0e
hanssolo2 <i11538@mvrht.com>
2017-02-09 22:24:44 +0100

    extended gladiator

--- Gladiatorkampf/src/main/java/de/twins/gladiator/domain/Gladiator.java
+++ Gladiatorkampf/src/main/java/de/twins/gladiator/domain/Gladiator.java
@@ -1,7 +1,57 @@
 package de.twins.gladiator.domain;
 
+import java.math.BigDecimal;
+
 public class Gladiator {
 
 	private String name;
+	private BigDecimal hp;
+	private BigDecimal attack;
+	private BigDecimal defense;
 
+	public Gladiator(String name, BigDecimal hp, BigDecimal attack, BigDecimal defense) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getHp() {
+		return hp;
+	}
+	public void setHp(BigDecimal hp) {
+		if(hp.compareTo(BigDecimal.ZERO) <= 0){
+		this.hp = BigDecimal.ZERO;
+		}
+		this.hp = hp;
+	}
+	public BigDecimal getAttack() {
+		return attack;
+	}
+	public void setAttack(BigDecimal attack) {
+		this.attack = attack;
+	}
+	public BigDecimal getDefense() {
+		return defense;
+	}
+	public void setDefense(BigDecimal defense) {
+		this.defense = defense;
+	}
+	/**
+	 * Reduces enemy attack power by own defense value.
+	 * The reduced attack power substracts directly the hp.
+	 * @param attack The attack power which reduces the hp.
+	 */
+	public void defend(BigDecimal attack){
+		BigDecimal dmg = attack.subtract(defense);
+		if(dmg.compareTo(BigDecimal.ZERO) > 0){
+			setHp(getHp().subtract(dmg));
+		}
+	}
 }
---
+++ Gladiatorkampf/src/test/java/de/twins/gladiator/domain/GladiatorTest.java
@@ -0,0 +1,24 @@
+package de.twins.gladiator.domain;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+
+public class GladiatorTest {
+
+	Gladiator testee;
+	Gladiator enemy;
+	
+	@Before
+	public void setup(){
+	testee = new Gladiator("testee",new BigDecimal(100),new BigDecimal(10),new BigDecimal(10));
+	enemy = new Gladiator("enemy",new BigDecimal(100),new BigDecimal(10),new BigDecimal(10));
+	}
+	public void shouldSetHpToZeroWhenHpIsNegative(){
+		testee.setHp(new BigDecimal(-1));
+		assertThat(testee.getHp(), is(BigDecimal.ZERO));
+	}
+}
