230f02f448df3dba9811334a2e42977b113fa4d0
hanssolo2 <i11538@mvrht.com>
2017-02-09 23:04:38 +0100

    extended tests

--- Gladiatorkampf/src/test/java/de/twins/gladiator/domain/GladiatorTest.java
+++ Gladiatorkampf/src/test/java/de/twins/gladiator/domain/GladiatorTest.java
@@ -12,15 +12,40 @@ public class GladiatorTest {
 
 	Gladiator testee;
 	Gladiator enemy;
-	
+
 	@Before
-	public void setup(){
-	testee = new Gladiator("testee",new BigDecimal(100),new BigDecimal(10),new BigDecimal(10));
-	enemy = new Gladiator("enemy",new BigDecimal(100),new BigDecimal(10),new BigDecimal(10));
+	public void setup() {
+		testee = new Gladiator("testee", new BigDecimal(100), new BigDecimal(10), new BigDecimal(10));
+		enemy = new Gladiator("enemy", new BigDecimal(100), new BigDecimal(10), new BigDecimal(10));
 	}
+
 	@Test
-	public void shouldSetHpToZeroWhenHpIsNegative(){
+	public void shouldSetHpToZeroWhenHpIsNegative() {
 		testee.setHp(new BigDecimal(-1));
 		assertThat(testee.getHp(), is(BigDecimal.ZERO));
+	}
+
+	@Test
+	public void shouldNullifyAttackWhenDefenseIsEqualsOrHigher() {
+		testee.defend(enemy.getAttack());
+		assertThat(testee.getHp(), is(new BigDecimal(100)));
+	}
+	@Test
+	public void shouldLoseHpFromAttack() {
+		enemy.setAttack(new BigDecimal(20));
+		testee.defend(enemy.getAttack());
+		assertThat(testee.getHp(), is(new BigDecimal(90)));
+	}
+	@Test
+	public void shouldSetName() {
+		String name = "gladiator";
+		testee.setName(name);
+		assertThat(testee.getName(), is(name));
+	}
+	@Test
+	public void shouldSetDefense() {
+		BigDecimal defense = new BigDecimal(3.33);
+		testee.setDefense(defense);
+		assertThat(testee.getDefense(), is(defense));
 	}
 }
